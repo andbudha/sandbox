94acd041d47e7f04d006767873331f47cd4e3fd4
andbudha <andreibartov@gmail.com>
2022-12-28 22:18:32 +0100

    sandbox updated

---
+++ src/components/Input.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Input = props => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+Input.propTypes = {
+
+};
