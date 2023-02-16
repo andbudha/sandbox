c09c969e89622180d0a64e2bc3b106f05e44a189
andbudha <andreibartov@gmail.com>
2023-02-16 11:48:13 +0100

    practicing 09-TDD /updated the added todolist ID checking

--- src/09_state/todolist-tasks-reducers.test.ts
+++ src/09_state/todolist-tasks-reducers.test.ts
@@ -4,6 +4,34 @@ import {AddToDoListAC, TodolistReducer} from "../08_todolist_tests_on_reducer/to
 
 
 test('The targeted IDs must be equal!', ()=>{
+
+    const startTasksState: TaskStateType = {};
+    const startToDoListState: ToDoListType[] = [];
+
+    const title = 'New To Do List';
+    const action = AddToDoListAC(title);
+
+    const endTasksState = tasksReducer(startTasksState, action);
+    const endToDoListState = TodolistReducer(startToDoListState, action);
+
+    const keys = Object.keys(endTasksState);
+    const firstTaskReducerID = keys[0];
+    const firstToDoListReducerID = endToDoListState[0].id;
+
+    expect(firstTaskReducerID).toBe(action.payload.todolistIDFromToDoListReducer);
+    expect(firstToDoListReducerID).toBe(action.payload.todolistIDFromToDoListReducer);
+})
+
+
+
+
+
+
+
+
+
+{/*
+test('The targeted IDs must be equal!', ()=>{
     const startTaskState: TaskStateType = {};
     const startToDoListState: ToDoListType[] = [];
 
@@ -21,3 +49,6 @@ test('The targeted IDs must be equal!', ()=>{
     expect(firstListIDFromTasks).toBe(action.payload.todolistIDFromToDoListReducer);
     expect(firstListIDFromToDoLists).toBe(action.payload.todolistIDFromToDoListReducer);
 })
+
+
+*/}
