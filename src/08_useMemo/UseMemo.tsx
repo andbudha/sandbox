af9335ee6e60d35951c7949dbdea91b905c621e7
andbudha <andreibartov@gmail.com>
2023-02-18 22:36:04 +0100

    practicing using useMemo/React.memo

--- src/08_useMemo/UseMemo.tsx
+++ src/08_useMemo/UseMemo.tsx
@@ -51,3 +51,89 @@ const UseMemo = () => {
 };
 
 export default UseMemo;
+
+
+{/*
+
+
+//just a copy for practicing
+
+import React, {useMemo, useState} from 'react';
+
+
+type UserType = {
+    name: string
+    age: number
+}
+type UserPropsType = {
+    users: UserType[]
+}
+
+
+
+
+type MessageCounterType = {
+    counterValue: number
+}
+const MessageCounter = (props: MessageCounterType) => {
+    return(
+        <div>
+            {props.counterValue}
+        </div>
+    );
+}
+
+const UsersSecret = (props: UserPropsType) => {
+
+    console.log('users rendered!')
+    return (
+        <div>
+            <ul>
+                {props.users.map((user, index)=>{
+                    return(
+                        <li key={index}>{user.name}</li>
+                    );
+                })}
+            </ul>
+
+        </div>
+    );
+}
+
+
+const Users = React.memo(UsersSecret);
+
+
+
+export const UseMemoTwo = () => {
+
+
+    const[users, setUsers] = useState([
+        {name: 'Andrei', age: 40},
+        {name: 'Delia', age: 5},
+        {name: 'Natalia', age: 37},
+        {name: 'Dorian', age: 1}
+    ]);
+
+    const[counterValue, setCounterValue]=useState(0);
+
+    console.log('rendered',counterValue);
+
+    const newArray = useMemo(()=>{
+        const newArray = users.filter(user=>user.name.toLowerCase().indexOf('n') > -1);
+        return newArray;
+    }, [])
+
+    return(
+        <div className={'memo_container'}>
+            <h2>useMemo 2 - Practice</h2>
+            <MessageCounter counterValue={counterValue}/>
+            <button onClick={()=>{setCounterValue(counterValue+1)}}>Increment Number</button>
+            <Users users={newArray}/>
+        </div>
+    );
+};
+
+
+
+*/}
--- src/08_useMemo/UseMemoTwo.tsx
+++ src/08_useMemo/UseMemoTwo.tsx
@@ -1,21 +1,43 @@
 import React, {useMemo, useState} from 'react';
 
 
-type UserType = {
-    name: string
-    age: number
-}
-type UserPropsType = {
-    users: UserType[]
-}
+
+export const UseMemoTwo = () => {
+
+    const[users, setUsers]=useState([
+        {name: 'Andrei', age: 40},
+        {name: 'Delia', age: 5},
+        {name: 'Natalia', age: 37},
+        {name: 'Dorian', age: 1}
+    ]);
+
+    const userArray = useMemo(()=>{
+        return users.filter(user=>user.name.toLowerCase().indexOf('n')>-1);
+
+    }, [])
+
+    console.log('Users rendered:', users.length);
+    const[counterValue, setCounterValue]=useState(0);
+
+    console.log('CounterValue - rendered:', counterValue);
+
+    return (
+        <div>
+            <h2>Message Count</h2>
+            <MessageCount counterValue={counterValue}/>
+            <button onClick={()=>setCounterValue(counterValue+1)}>INCREASE USER-COUNT</button>
+            <Users users={userArray}/>
+        </div>
+    );
+};
 
 
 
-
-type MessageCounterType = {
+type MessageCountPropsType = {
     counterValue: number
 }
-const MessageCounter = (props: MessageCounterType) => {
+
+const MessageCount = (props: MessageCountPropsType) => {
     return(
         <div>
             {props.counterValue}
@@ -23,54 +45,31 @@ const MessageCounter = (props: MessageCounterType) => {
     );
 }
 
-const UsersSecret = (props: UserPropsType) => {
 
-    console.log('users rendered!')
-    return (
+
+
+
+type UserType = {
+    name: string
+    age: number
+}
+
+type UsersPropsType = {
+    users: UserType[]
+}
+const UsersSecret = (props: UsersPropsType) => {
+    return(
         <div>
             <ul>
                 {props.users.map((user, index)=>{
+                    console.log(user.name);
                     return(
                         <li key={index}>{user.name}</li>
                     );
                 })}
             </ul>
-
         </div>
     );
 }
 
-
 const Users = React.memo(UsersSecret);
-
-
-
-export const UseMemoTwo = () => {
-
-
-    const[users, setUsers] = useState([
-        {name: 'Andrei', age: 40},
-        {name: 'Delia', age: 5},
-        {name: 'Natalia', age: 37},
-        {name: 'Dorian', age: 1}
-    ]);
-
-    const[counterValue, setCounterValue]=useState(0);
-
-    console.log('rendered',counterValue);
-
-    const newArray = useMemo(()=>{
-        const newArray = users.filter(user=>user.name.toLowerCase().indexOf('n') > -1);
-        return newArray;
-    }, [])
-
-    return(
-        <div className={'memo_container'}>
-            <h2>useMemo Practice</h2>
-            <MessageCounter counterValue={counterValue}/>
-            <button onClick={()=>{setCounterValue(counterValue+1)}}>Increment Number</button>
-            <Users users={newArray}/>
-        </div>
-    );
-};
-
